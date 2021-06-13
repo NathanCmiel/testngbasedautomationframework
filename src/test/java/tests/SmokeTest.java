32758aa6d3f2f3afcd957a93140cf7fe8b524610
Nathan Cmiel <cetan25@gmail.com>
2021-06-13 15:05:30 -0400

    Finished implementing ITestListener.

--- src/test/java/tests/SmokeTest.java
+++ src/test/java/tests/SmokeTest.java
@@ -2,11 +2,14 @@ package tests;
 
 import base.UITestBase;
 import org.testng.annotations.Test;
+import utility.TestListener;
 
 public class SmokeTest extends UITestBase {
 
     @Test
     public void verifyHomePageIsDisplayed() {
-
+        TestListener.step().info("User is on homepage.");
+        TestListener.step().info("User is about to login.");
+        TestListener.step().info("User is logged in.");
     }
 }
--- src/test/java/utility/TestListener.java
+++ src/test/java/utility/TestListener.java
@@ -1,41 +1,57 @@
 package utility;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class TestListener implements ITestListener {
 
+    private ExtentReports extent;
+    private ExtentSparkReporter spark;
+    private static ExtentTest extentTest;
+
     //--------- Test Context (Individual Test)
     @Override
     public void onStart(ITestContext context) {
+        extent = new ExtentReports();
+        String reportPath = System.getProperty("user.dir") + "/reports/result.html";
+        spark = new ExtentSparkReporter(reportPath);
 
+        extent.attachReporter(spark);
     }
 
     @Override
     public void onFinish(ITestContext context) {
-
+        extent.flush();
     }
 
     //--------- Individual Test Case
     @Override
     public void onTestStart(ITestResult result) {
+        String testCaseName = result.getMethod().getMethodName();
+        extentTest = extent.createTest(testCaseName);
+    }
 
+    public static ExtentTest step() {
+        return extentTest;
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
-
+        extentTest.pass("This test case has passed.");
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-
+        extentTest.pass("This test case has failed.");
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
-
+        extentTest.pass("This test case has skipped.");
     }
 
     @Override
